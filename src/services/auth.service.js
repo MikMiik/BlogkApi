e89d748cf02a492a9ef823efd1bab634a835489f
MikMik <minh0936532430@gmail.com>
2025-07-12 19:53:25 +0700

    update

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -2,25 +2,39 @@ const { User } = require("@/models");
 const { hashPassword, comparePassword } = require("@/utils/bcrytp");
 const jwtService = require("./jwt.service");
 const refreshTokenService = require("@/services/refreshToken.service");
+const queue = require("@/utils/queue");
 
 const register = async (data) => {
   const user = await User.create({
     ...data,
     password: await hashPassword(data.password),
   });
-  const tokenData = jwtService.generateAccessToken(user.id);
+  const tokenData = jwtService.generateMailToken(user.id);
   return { user, tokenData };
 };
 
-const login = async (email, password, rememberMe) => {
+const login = async (req, email, password, rememberMe) => {
   const user = await User.findOne({ where: { email } });
   if (!user) {
-    throw new Error("Thông tin đăng nhập không hợp lệ.");
+    throw new Error("Invalid login information.");
+  }
+
+  if (!user.dataValues.verifiedAt) {
+    const tokenData = jwtService.generateMailToken(user.dataValues.id);
+    const verifyUrl = `${req.protocol}://${req.host}/api/v1/auth/verify-email?token=${tokenData.token}`;
+    queue.dispatch("sendVerifyEmailJob", {
+      userId: user.id,
+      token: tokenData.token,
+      verifyUrl,
+    });
+    throw new Error(
+      "Your account has not verified yet, please check your email to verify"
+    );
   }
 
   const isValid = await comparePassword(password, user.password);
   if (!isValid) {
-    throw new Error("Thông tin đăng nhập không hợp lệ.");
+    throw new Error("Invalid login information");
   }
 
   const tokenData = jwtService.generateAccessToken(user.id);
