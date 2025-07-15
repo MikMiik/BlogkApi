873aab57903923ec0ed2cbed3c7c2282839acf2c
MikMik <minh0936532430@gmail.com>
2025-07-15 19:33:21 +0700

    update

--- src/configs/database.js
+++ src/configs/database.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 require("module-alias/register");
 require("dotenv").config();
+console.log(process.env.DB_USER);
 
 module.exports = {
   development: {
--- src/configs/env.js
+++ src/configs/env.js
@@ -3,7 +3,6 @@ import fs from "fs";
 
 // Xác định file .env cần dùng dựa vào biến môi trường NODE_ENV
 const envFile = `.env.${process.env.NODE_ENV || "local"}`;
-
 // Kiểm tra xem file đó có tồn tại không
 if (fs.existsSync(envFile)) {
   dotenv.config({ path: envFile }); // Load các biến môi trường từ file đó
