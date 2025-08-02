a916ddcf6c3700b0ead42289f0b56ac98a40d951
MikMik <minh0936532430@gmail.com>
2025-08-02 13:35:19 +0700

    fix upload

--- server.js
+++ server.js
@@ -62,14 +62,6 @@ app.set("views", "./src/views");
 app.set("layout", "./layouts/default");
 
 // Router
-app.use(
-  "/api/v1/uploads",
-  express.static(path.join(__dirname, "public/uploads"))
-);
-app.use(
-  "/api/v1/favicon",
-  express.static(path.join(__dirname, "public/favicon"))
-);
 app.use("/api/v1", checkAuth, setContext, router);
 
 // ErrorHandle
--- src/middlewares/handleUpload.js
+++ src/middlewares/handleUpload.js
@@ -1,13 +1,22 @@
 const multer = require("multer");
 const path = require("path");
+const fs = require("fs");
+
+const uploadDir = path.join(__dirname, "../../public/uploads");
+
+// Tạo thư mục nếu chưa tồn tại
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "public/uploads");
+    cb(null, uploadDir);
   },
   filename: (req, file, cb) => {
     const ext = path.extname(file.originalname);
     const filename = Date.now() + "-" + file.fieldname + ext;
+
     cb(null, filename);
   },
 });
